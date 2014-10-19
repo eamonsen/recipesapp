17606849369b1f63fbe1a3518f22f2bf07ff5a02
Liz Monsen <eamonsen@gmail.com>
2014-10-18 22:02:12 -0700

    trying alpaca for the new recipe form, typeahead not working

--- recipesapp.js
+++ recipesapp.js
@@ -1,4 +1,9 @@
 Recipes = new Mongo.Collection("recipes");
+cuisineList = _.uniq(Recipes.find({}, {
+    sort: {cuisine: 1}, fields: {cuisine: true}
+}).fetch().map(function(x) {
+    return x.cuisine;
+}), true);
 
 
 if (Meteor.isClient) {
@@ -18,20 +23,93 @@ if (Meteor.isClient) {
     useFontAwesome: true}
   }
 
-  //Adding formfor adding new recipes to the db
+  //Adding form for adding new recipes to the db
   Template.addButton.events({
     'click .add': function() {
+      var newName = $('#newName').val();
+      //var newIngredients =
+      //var newType = 
+      //var newCuisine = 
+      Recipes.insert({name: newName});
     //Recipes.insert({
-      console.log('hello')
       console.log($('#newName').val())
 
     //})
   }
 })
-}
 
+  var schema = {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string"
+            },
+            "cuisine": {
+                "type": "string",
+            },
+            "kind": {
+                "type": "string"
+            },
+            "ingredients": {
+                "type": "string",
+            }
+        }
+    };
 
+    console.log(cuisineList);
 
+  
+  var isFormRendered = false
+ Template.addRecipe.events({
+    'click .new': function() {
+      cuisineList = _.uniq(Recipes.find({}, {
+        sort: {cuisine: 1}, fields: {cuisine: true}
+      }).fetch().map(function(x) {
+        return x.cuisine;
+      }), true);
+       var options = {
+        "fields": {
+            "name": {
+                "type": "text",
+                "label": "Recipe Name",
+                "id": "newName"
+            },
+            "cuisine": {
+                "type": "text",
+                "label": "Cuisine",
+                "id": "newCuisine",
+                "typeahead": {
+                  "name": "cuisine",
+                  "template": "<p style='color: blue'>{{cuisine}}</p>",
+                  "local": ["merican", "Asian Fusion", "Italian", "Jewish", "Lebanese"],
+                  }
+            },
+            "kind": {
+                "type": "text",
+                "label": "Type of Dish",
+                "id": "newType",
+            },
+            "ingredients": {
+                "type": "text",
+                "label": "Ingredient List",
+                "id": "newIngredients"
+            },
+          }
+        };  
+
+      if(!isFormRendered){
+        $("#recipeForm").alpaca({
+        "schema": schema,
+        "options": options,
+        "ui": "bootstrap"
+      });
+        isFormRendered = true
+      }
+      
+    }
+  })
+}   
+    
 if (Meteor.isServer) {
     
 }
